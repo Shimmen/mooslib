6df13966523620040e3c55db72bee2f7600d3017
Simon Moos <simon.moos95@gmail.com>
2020-05-10 18:36:31 +0200

    Add XYZ, sRGB, and HSV conversions

--- include/mooslib.h
+++ include/mooslib.h
@@ -1134,7 +1134,12 @@ struct aabb3 {
 // TODO: vector to spherical, vice versa
 
 // Color utilities
-// TODO: HSV to RGB, sRGB linear to display conversion, ACES tonemap & some utilities etc.
+
+Float luminance(const vec3& color)
+{
+    constexpr vec3 luma = vec3(0.2126, 0.7152, 0.0722);
+    return dot(color, luma);
+}
 
 namespace ACES {
 
@@ -1169,6 +1174,118 @@ namespace ACES {
         color = outputMatrix * color;
         color = clamp(color, vec3(0.0), vec3(1.0));
         return color;
+    }
+}
+
+namespace colorspace {
+
+    const mat3 linear_sRGB_to_XYZ = mat3(
+        { 0.4123865632529917, 0.21263682167732384, 0.019330620152483987 },
+        { 0.35759149092062537, 0.7151829818412507, 0.11919716364020845 },
+        { 0.18045049120356368, 0.07218019648142547, 0.9503725870054354 });
+
+    const mat3 XYZ_to_linear_sRGB = mat3(
+        { 3.2410032329763587, -0.9692242522025166, 0.055639419851975444 },
+        { -1.5373989694887855, 1.875929983695176, -0.20401120612390997 },
+        { -0.4986158819963629, 0.041554226340084724, 1.0571489771875335 });
+
+    namespace sRGB {
+        Float gammaEncode(Float linear)
+        {
+            // (i.e. convert from linear sRGB to gamma-encoded sRGB)
+            return (linear < 0.0031308)
+                ? 12.92 * linear
+                : 1.055 * std::pow(linear, 1.0 / 2.4) - 0.055;
+        }
+
+        Float gammaDecode(Float encoded)
+        {
+            // (i.e. convert from gamma-encoded sRGB to linear sRGB)
+            return (encoded < 0.04045)
+                ? encoded / 12.92
+                : std::pow((encoded + 0.055) / 1.055, 2.4);
+        }
+
+        vec3 gammaEncode(const vec3& linear)
+        {
+            return { gammaEncode(linear.x), gammaEncode(linear.y), gammaEncode(linear.z) };
+        }
+
+        vec3 gammaDecode(const vec3& encoded)
+        {
+            return { gammaDecode(encoded.x), gammaDecode(encoded.y), gammaDecode(encoded.z) };
+        }
+    }
+
+    namespace HSV {
+
+        vec3 toRGB(const vec3& hsv)
+        {
+            // From https://en.wikipedia.org/wiki/HSL_and_HSV#HSV_to_RGB
+
+            // make sure H is in range [0, 360] degrees
+            Float h = hsv.x;
+            h = std::fmod(h, 360.0);
+            h += 360.0;
+            h = std::fmod(h, 360.0);
+
+            const Float& s = hsv.y;
+            const Float& v = hsv.z;
+
+            Float c = v * s;
+            Float hPrim = h / 60.0;
+            Float x = c * (1.0 - std::abs(std::fmod(hPrim, 2.0) - 1.0));
+            Float m = v - c;
+
+            if (hPrim <= 1.0)
+                return { c + m, x + m, m };
+            if (hPrim <= 2.0)
+                return { x + m, c + m, m };
+            if (hPrim <= 3.0)
+                return { m, c + m, x + m };
+            if (hPrim <= 4.0)
+                return { m, x + m, c + m };
+            if (hPrim <= 5.0)
+                return { x + m, m, c + m };
+            if (hPrim <= 6.0)
+                return { c + m, m, x + m };
+
+            MOOSLIB_ASSERT(false);
+            return { 0.0, 0.0, 0.0 };
+        }
+
+        vec3 fromRGB(vec3 rgb)
+        {
+            // From https://en.wikipedia.org/wiki/HSL_and_HSV#From_RGB
+
+            rgb = clamp(rgb, vec3(0.0), vec3(1.0));
+            const Float& r = rgb.x;
+            const Float& g = rgb.y;
+            const Float& b = rgb.z;
+
+            Float xMax = std::max(r, std::max(g, b));
+            Float xMin = std::min(r, std::min(g, b));
+            Float c = xMax - xMin;
+
+            const Float& v = xMax;
+            Float s = (v == 0.0)
+                ? 0.0
+                : c / v;
+
+            Float h = 0.0;
+            if (c == 0.0)
+                h = 0.0;
+            else if (v == r)
+                h = 60.0 * (g - b) / c;
+            else if (v == g)
+                h = 60.0 * (2.0 + (b - r) / c);
+            else if (v == b)
+                h = 60.0 * (4.0 + (r - g) / c);
+            else
+                MOOSLIB_ASSERT(false);
+
+            return { h, s, v };
+        }
     }
 }
 
